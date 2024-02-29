98750911ec2ed5a175af0cdfd525bbd433ad03a2
agreeable frog <theicreeps2@gmail.com>
2024-02-29 19:25:20 +0100

    [DEV] first structure

---
+++ renderer/src/main.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
+#include <GL/glew.h>
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+}
