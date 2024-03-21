1ef7eff5a250e2721044d8a6c0a76af97ee66ce6
agreeable frog <theicreeps2@gmail.com>
2024-03-21 18:30:21 +0100

    [DEV] framebuffer implem

--- renderer/src/framebuffer.cc
+++ renderer/src/framebuffer.cc
@@ -1,1 +1,40 @@
 #include "framebuffer.hh"
+
+void Framebuffer::init() {
+    glGenFramebuffers(1, &_id);
+}
+
+Framebuffer::Framebuffer() {
+    init();
+}
+
+Framebuffer::Framebuffer(const Framebuffer& other) {
+    init();
+}
+
+Framebuffer& Framebuffer::operator=(const Framebuffer& other) {
+    if (&other == this) {
+        return *this;
+    }
+    glDeleteFramebuffers(1, &_id);
+    init();
+    return *this;
+}
+
+bool Framebuffer::isComplete() const {
+    return (glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE);
+}
+
+void Framebuffer::bind() const {
+    glBindBuffer(GL_FRAMEBUFFER, _id);
+}
+
+void Framebuffer::unbind() const {
+    glBindBuffer(GL_FRAMEBUFFER, 0);
+}
+
+void Texture::init() {
+    glGenTextures(1, &_id);
+    glBindTexture(GL_TEXTURE_2D, _id);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 900, 600, 0, GL_RGB, GL_UNSIGNED_BYTE, 0);
+}
