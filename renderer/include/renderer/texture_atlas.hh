91fcb9e5361368a7389922289ab1b2e89053b523
agreeable frog <theicreeps2@gmail.com>
2024-04-06 10:55:31 +0200

    [DEV] texture atlas tmp

--- renderer/include/renderer/texture_atlas.hh
+++ renderer/include/renderer/texture_atlas.hh
@@ -7,30 +7,42 @@
 #include <string>
 #include <vector>
 
+class TextureAtlas;
+
+class AtlasTexture {
+public:
+    AtlasTexture(const std::string& path);
+    AtlasTexture(std::vector<u_char> data, size_t width, size_t height,
+                 GLenum format);
+
+private:
+    bool _bound = false;
+    std::weak_ptr<TextureAtlas> _atlas;
+    std::vector<u_char> data;
+    size_t _width;
+    size_t _height;
+    GLenum _format;
+    size_t _atlasIndex;
+};
+
 class TextureAtlas {
 public:
     TextureAtlas();
     ~TextureAtlas();
     TextureAtlas(const TextureAtlas&) = delete;
     TextureAtlas& operator=(const TextureAtlas&) = delete;
-    bool addTexture(const std::string& path);
-    bool addTexture(std::vector<u_char> data, size_t width, size_t height, GLenum _format);
+    bool addTexture(std::shared_ptr<AtlasTexture> pAtlasTexture);
+    void build();
     void bind() const;
     void unbind() const;
+
 private:
+    bool _built = false;
+    std::vector<std::shared_ptr<AtlasTexture>> _boundTextures;
     static GLuint _currentBound; // prevent useless rebinds
     GLuint _id;
     size_t _width;
     size_t _height;
     GLenum _format;
-};
-
-class AtlasedTexture {
-public:
-    AtlasedTexture();
-private:
-    std::weak_ptr<TextureAtlas> _atlas;
-    size_t _width;
-    size_t _height;
-    size_t _atlasIndex;
+    size_t size;
 };
